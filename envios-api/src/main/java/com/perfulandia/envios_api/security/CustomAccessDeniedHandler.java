90395373a4fa8b4cd8ca93a44c9d498de60ec24e
RatonUniversitario-gift <iga.pizarro@duocuc.cl>
2025-06-23 00:03:51 -0400

    se implementan endpoints cupon y envios a api gateway, se aplica seguridad a cupones y envios

---
+++ envios-api/src/main/java/com/perfulandia/envios_api/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,23 @@
+package com.perfulandia.envios_api.security;
+
+import java.io.IOException;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException {
+
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json");
+        response.getWriter().write("{\"codigo\": 403, \"mensaje\": \"No tienes permisos suficientes para acceder a este recurso.\"}");
+    }
+}
