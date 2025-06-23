b71f5cd0d6d5dcf37eb175c79664b4aaf68d6c76
Leonel Toro <128005116+Leonel-Toro@users.noreply.github.com>
2025-06-23 01:20:55 -0400

    fix:Se corrijen errores menores en gateway y api ventas

--- ventas-api/src/main/java/com/perfulandia/ventas_api/security/RestClientConfig.java
+++ ventas-api/src/main/java/com/perfulandia/ventas_api/security/RestClientConfig.java
@@ -1,6 +1,5 @@
 package com.perfulandia.ventas_api.security;
 
-import com.clientes_api.security.JwtRestTemplateInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
